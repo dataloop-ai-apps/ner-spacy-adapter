28cacca6f19f89dd5ba770783da391b735958f01
Or Shabtay <or@dataloop.ai>
2024-08-08 18:30:09 +0300

    added spliting for long text

--- adapter.py
+++ adapter.py
@@ -14,28 +14,28 @@ class Adapter(dl.BaseModelAdapter):
 
     def predict(self, batch, **kwargs):
         batch_annotations = list()
+        split_str = ". "
         for text in batch:
-            doc = self.nlp(text)
+            offset = 0
             collection = dl.AnnotationCollection()
-            for entity in doc.ents:
-                collection.add(dl.Text(text_type='block',
-                                       label=entity.label_,
-                                       start=entity.start_char,
-                                       end=entity.end_char),
-                               model_info={'name': self.model_entity.name,
-                                           'confidence': 1.})
+            # split by ". " and keep the same char in the chunks
+            sentences = text.split(split_str)
+            for sentence in sentences:
+                sentence += split_str
+                results = self.nlp(sentence)
+                for entity in results.ents:
+                    # print("=========")
+                    # print(entity.label_)
+                    # print(entity.text)
+                    # print(entity.start_char + offset)
+                    # print(entity.end_char + offset)
+                    collection.add(dl.Text(text_type='block',
+                                           label=entity.label_,
+                                           start=entity.start_char + offset,
+                                           end=entity.end_char + offset
+                                           ),
+                                   model_info={'name': self.model_entity.name,
+                                               'confidence': 1.})
+                offset += len(sentence)
             batch_annotations.append(collection)
         return batch_annotations
-
-
-if __name__ == "__main__":
-    adapter = Adapter()
-    # adapter.predict_items(items=["66b4856680ee7d69c26cd4e8"])
-    adapter.load(None)
-    doc = adapter.nlp(
-        "On April 2, 2023, Apple IncInc., a leading technology company, announced the launch of its newest smartphone, the iPhone 14 Pro MaMax. The device features a groundbreaking new camera system and a powerful A16 Bionic chip. It will be available for pre-order on April 9, 2023, and in stores starting April 16, 2023. The price for the base model starts at $999. Meanwhile, in the realm of politics, US President Joe Biden is scheduled to visit France on May 15, 2023, for a summit with French President Emmanuel Macron to discuss climate change and global security. The meeting is expected to be held at the Élysée Palace in")
-    for entity in doc.ents:
-        print(entity.label_)
-        print(entity.text)
-        print(entity.start_char)
-        print(entity.end_char)
